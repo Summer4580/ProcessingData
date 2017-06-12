6ff7c9df6f2095ddfbc7fd6c4456e70d8fe6c95f
Summer4580 <20161104580X@mac01.local>
2017-06-12 08:17:27 +0800

    Initial Commit

---
+++ ProcessingData/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ProcessingData
+//
+//  Created by Summer on 2017/6/12.
+//  Copyright © 2017年 Summer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
