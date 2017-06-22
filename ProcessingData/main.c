448fe288af78485b8b5b75e3423c0d3b7e29fa1c
Summer4580 <20161104580X@Mac01.local>
2017-06-22 09:57:30 +0800

    GPS文件读取
    简单提取数据

--- ProcessingData/main.c
+++ ProcessingData/main.c
@@ -7,9 +7,70 @@
 //
 
 #include <stdio.h>
+int main()
+{
+    char a[63];
+    char b[63];
+    FILE *fp;
+    fp=fopen("//Users//20161104580X//Desktop//GPS文档//GPS170510.log","r");
+    while(!feof(fp))
+    {
+        fscanf(fp,"%s",a);
+        fscanf(fp,"%s",b);
+    }
+    printf("GPGGA时间：");
+    int i=7;
+    while(b[i]!=',')
+    {
+        printf("%c",b[i]);
+        i++;
+    }
+    printf("\n海拔=");
+    i=43;
+    while(b[i]!=',')
+    {
+        printf("%c",b[i]);
+        i++;
+    }
+    printf("\n维度=");
+    i=14;
+    while(b[i]!=',')
+    {
+        printf("%c",b[i]);
+        i++;
+    }
+    printf("%c\n",b[i+1]);
+    i=i+3;
+    printf("经度=");
+    while(b[i]!=',')
+    {
+        printf("%c",b[i]);
+        i++;
+    }
+    printf("%c\n",b[i+1]);
+    printf("GPRMC时间:\n");
+    i=7;
+    while(a[i]!=',')
+    {
+        printf("%c",a[i]);
+        i++;
+    }
+    printf("\n维度=");
+    i=16;
+    while(a[i]!=',')
+    {
+        printf("%c",a[i]);
+        i++;
+    }
+    printf("%c\n",a[i+1]);
+    i=i+3;
+    printf("经度=");
+    while(a[i]!=',')
+    {
+        printf("%c",a[i]);
+        i++;
+    }
+    printf("%c\n",a[i+1]);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
