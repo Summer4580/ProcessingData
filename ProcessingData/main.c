a559663d548a51ddd463d0f632b96cb98e68bc20
Summer4580 <20161104580X@Mac01.local>
2017-06-26 11:59:00 +0800

    GPS数据读取
    使用链表存放数据

--- ProcessingData/main.c
+++ ProcessingData/main.c
@@ -7,70 +7,93 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct GPS_type{
+    char time[20];
+    char date[20];
+    char lotd[20];
+    char lotdx;
+    char latd[20];
+    char latdx;
+    char hingt[20];
+    struct GPS *next;
+}GPS;
+void GPRMC(char a[],GPS *Temp)
+{
+//    printf("GPRMC日期:");
+    int i=51,j=0;
+    while(a[i]!=',')
+    {
+        Temp->date[j] = a[i];
+//        printf("%c",a[i]);
+        i++,j++;
+    }
+//    printf("\nGPRMC时间:");
+    i=7,j=0;
+    while(a[i]!=',')
+    {
+        Temp->time[j] = a[i];
+//        printf("%c",a[i]);
+        i++,j++;
+    }
+//    printf("\n维度=");
+    i=16,j=0;
+    while(a[i]!=',')
+    {
+        Temp->latd[j] = a[i];
+//        printf("%c",a[i]);
+        i++,j++;
+    }
+    Temp->latdx = a[i+1];
+//    printf("%c\n",a[i+1]);
+    i=i+3,j=0;
+//    printf("经度=");
+    while(a[i]!=',')
+    {
+        Temp->lotd[j] = a[i];
+//        printf("%c",a[i]);
+        i++,j++;
+    }
+    Temp->lotdx = a[i+1];
+//    printf("%c\n",a[i+1]);
+}
+void GPGGA(char b[],GPS *Temp)
+{
+//    printf("\n海拔=");
+      int i=43,j=0;
+//    while(b[i]!=',')
+//    {
+    Temp->hingt[j] = b[i];
+//        printf("%c",b[i]);
+        i++,j++;
+//    }
+
+}
 int main()
 {
     char a[63];
     char b[63];
     FILE *fp;
     fp=fopen("//Users//20161104580X//Desktop//GPS文档//GPS170510.log","r");
+    if(fp==NULL)
+        printf("打开文件错误!");
+    GPS *temp,*head,*s;
+    temp = (GPS *)malloc(sizeof(GPS));
+    temp->next = NULL;
+    s = (GPS *)malloc(sizeof(GPS));
+    s->next = NULL;
+    head = temp;
     while(!feof(fp))
     {
         fscanf(fp,"%s",a);
+        GPRMC(a,s);
         fscanf(fp,"%s",b);
+        GPGGA(b,s);
+        temp->next = s;
     }
-    printf("GPGGA时间：");
-    int i=7;
-    while(b[i]!=',')
+    while(head->next!=NULL)
     {
-        printf("%c",b[i]);
-        i++;
+        printf("日期:%s\n时间:%s\n经度:%s%c\n维度:%s%c\n海拔:%s\n",head->date,head->time,head->lotd,head->lotdx,head->latd,head->latdx,head->hingt);
     }
-    printf("\n海拔=");
-    i=43;
-    while(b[i]!=',')
-    {
-        printf("%c",b[i]);
-        i++;
-    }
-    printf("\n维度=");
-    i=14;
-    while(b[i]!=',')
-    {
-        printf("%c",b[i]);
-        i++;
-    }
-    printf("%c\n",b[i+1]);
-    i=i+3;
-    printf("经度=");
-    while(b[i]!=',')
-    {
-        printf("%c",b[i]);
-        i++;
-    }
-    printf("%c\n",b[i+1]);
-    printf("GPRMC时间:\n");
-    i=7;
-    while(a[i]!=',')
-    {
-        printf("%c",a[i]);
-        i++;
-    }
-    printf("\n维度=");
-    i=16;
-    while(a[i]!=',')
-    {
-        printf("%c",a[i]);
-        i++;
-    }
-    printf("%c\n",a[i+1]);
-    i=i+3;
-    printf("经度=");
-    while(a[i]!=',')
-    {
-        printf("%c",a[i]);
-        i++;
-    }
-    printf("%c\n",a[i+1]);
-
     return 0;
 }
